f2fae1a237f95f17a4ab8dacd0811f38ac906d13
Mykola Polotnianko <nikpolale@gmail.com>
2022-05-01 22:18:49 +0300

    feat: add react-navigation dependencies

--- app/android/app/src/main/java/com/yourgreatapp/MainActivity.java
+++ app/android/app/src/main/java/com/yourgreatapp/MainActivity.java
@@ -1,4 +1,5 @@
 package com.yourgreatapp;
+import android.os.Bundle;
 import expo.modules.ReactActivityDelegateWrapper;
 
 import com.facebook.react.ReactActivity;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "yourgreatapp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
