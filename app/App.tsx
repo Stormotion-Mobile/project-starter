924facadd6eb516dc4fbc503b852a2ca0c1875cb
Mykola Polotnianko <nikpolale@gmail.com>
2022-05-07 15:42:06 +0300

    add i18next initialize

--- app/App.tsx
+++ app/App.tsx
@@ -4,6 +4,17 @@ import React, {memo} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import useRootApolloClient from './src/hooks/useRootApolloClient';
 import RootNavigator from './src/navigation/RootNavigator';
+import {initReactI18next} from 'react-i18next';
+import i18n from 'i18next';
+import en from './src/strings/en.json';
+
+i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  fallbackLng: 'en',
+  resources: {
+    en,
+  },
+});
 
 const App = () => {
   const client = useRootApolloClient();
