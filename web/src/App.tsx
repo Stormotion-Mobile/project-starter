582347036302790f7ecf7d8acd32a3040c488a01
Mykola Polotnianko <nikpolale@gmail.com>
2022-05-07 16:58:41 +0300

    feat: inject root apollo client to the app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,25 @@
+import {ApolloProvider} from '@apollo/client';
+import {useMemo} from 'react';
+import Loading from './components/Loading';
+import useRootApolloClient from './hooks/useRootApolloClient';
 import RootNavigator from './navigation/RootNavigator';
 
 function App() {
-  return <RootNavigator />;
+  const client = useRootApolloClient();
+
+  const Content = useMemo(
+    () =>
+      client ? (
+        <ApolloProvider client={client}>
+          <RootNavigator />
+        </ApolloProvider>
+      ) : (
+        <Loading />
+      ),
+    [client],
+  );
+
+  return Content;
 }
 
 export default App;
