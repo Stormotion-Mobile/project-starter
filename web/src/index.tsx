313fa38c3658b5f15255c34be2e9960bfa51dd4d
Mykola Polotnianko <nikpolale@gmail.com>
2022-05-07 16:33:19 +0300

    feat(web): add i18n

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import initI18n from './initI18n';
+
+initI18n();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
---
+++ web/src/initI18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import {initReactI18next} from 'react-i18next';
+import en from './strings/en.json';
+
+const initI18n = () =>
+  i18n
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      debug: true,
+      fallbackLng: 'en',
+      resources: {
+        en,
+      },
+    });
+
+export default initI18n;
