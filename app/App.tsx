d5e758b2c33373baaa24710175ff08ce437c208d
Mykola Polotnianko <nikpolale@gmail.com>
2022-05-07 15:07:35 +0300

    feat: add apollo provider

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,19 @@
+import {ApolloProvider} from '@apollo/client';
 import {NavigationContainer} from '@react-navigation/native';
 import React, {memo} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import useRootApolloClient from './src/hooks/useRootApolloClient';
 import RootNavigator from './src/navigation/RootNavigator';
 
 const App = () => {
+  const client = useRootApolloClient();
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <RootNavigator />
+        <ApolloProvider client={client}>
+          <RootNavigator />
+        </ApolloProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
